72248445c684f99d30f3a1929d839ac1f368f2eb
Swetha Chavalla <swethachavalla@gmail.com>
2016-10-20 18:14:23 +0530

    Added Date Method

--- installing/src/myfinance/logindetails.java
+++ installing/src/myfinance/logindetails.java
@@ -1,5 +1,12 @@
 package myfinance;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -7,6 +14,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
 
 public class logindetails {
 	String URL;
@@ -50,6 +62,37 @@ public class logindetails {
 
 	public void logout(WebDriver driver) {
 		driver.findElement(By.xpath(".//*[@id='bs-example-navbar-collapse-1']/ul/li[5]/span/a")).click();
+	}
+	
+	public void Date(WebDriver driver) throws InterruptedException, BiffException, IOException{
+		File fs = new File("C:/Users/Swetha/Desktop/IMA Testing/All Financial Scenarios Test Data.xls");
+		Workbook ws = Workbook.getWorkbook(fs);
+		Sheet s = ws.getSheet("Date");
+		int rows = s.getRows();
+		int columns = s.getColumns();
+		String inputdata[][] = new String[rows-1][columns];
+		for (int i = 1; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				Cell cl = s.getCell(j, i);
+				inputdata[i-1][j] = cl.getContents();
+			}
+		}
+		
+		for (int i = 0; i <rows-1; i++) {
+			int year = Integer.parseInt(inputdata[i][0]);
+			int month = Integer.parseInt(inputdata[i][1]);
+			int day = Integer.parseInt(inputdata[i][2]);
+		Calendar c = Calendar.getInstance();
+		c.set(Calendar.YEAR, year);
+		c.set(Calendar.MONTH, month);
+		c.set(Calendar.DAY_OF_MONTH, day);
+		DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+		Date datespecified = c.getTime();
+		driver.findElement(By.id("date")).clear();
+		Thread.sleep(2000);
+		driver.findElement(By.id("date")).sendKeys(df.format(datespecified));
+		Thread.sleep(2000);
+	}
 	}
 
 	public void forgotuserid(WebDriver driver) {
