8208c0f517e10095c89be669efe641cd23850342
Swetha Chavalla <swethachavalla@gmail.com>
2016-10-23 17:44:05 +0530

    Added new Login method.

--- installing/src/myfinance/FinanceTestingIMA_TC_004.java
+++ installing/src/myfinance/FinanceTestingIMA_TC_004.java
@@ -1,11 +1,16 @@
 package myfinance;
 
+import java.io.File;
 import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
 import jxl.read.biff.BiffException;
 
 public class FinanceTestingIMA_TC_004 {
@@ -15,14 +20,29 @@ public class FinanceTestingIMA_TC_004 {
 	FinancialVouchersDEMO12 help2 = new FinancialVouchersDEMO12();
 	
 	@Test(priority = 1, dataProvider = "logintestdata")
-	public void Login(String username, String password, String URL) throws InterruptedException {
-		ldr.adminlogin(driver, username, password);
-		driver.navigate().to(URL);
-		Thread.sleep(3000);
+	public void Login(String URL,String username, String password) throws InterruptedException {
+		ldr.adminlogin(driver,URL, username, password);
+		Thread.sleep(2000);
 	}
 	@Test(priority=2)
 	public void AdvancePayment() throws BiffException, InterruptedException, IOException{
 		help2.advancepayment(driver);
 	}
-
+	@DataProvider    (name = "logintestdata")
+    public  Object [][] readexcel()  throws  IOException, BiffException  {
+       File fs = new File("C:/Users/Swetha/Desktop/IMA Testing/All Financial Scenarios Test Data.xls");
+        Workbook ws= Workbook.getWorkbook(fs);
+        Sheet s = ws.getSheet("Logindata");
+        int rows = s.getRows();
+        int columns = s.getColumns();
+        String inputdata [][]= new String [rows-1][columns]; 
+        for (int i=1; i<rows; i++){
+            for (int j=0; j<columns; j++){
+                Cell cl = s.getCell(j,i);
+                inputdata [i-1][j] = cl.getContents();
+                      
+            }
+        }
+                return inputdata;
+	}
 }
