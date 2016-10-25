6b12dc15f5ac485c7d2f711e3541a322a1785a3d
Swetha Chavalla <swethachavalla@gmail.com>
2016-10-25 22:14:46 +0530

    Nothing much changes.

--- installing/src/myfinance/BookingFacilityhelpdemo12.java
+++ installing/src/myfinance/BookingFacilityhelpdemo12.java
@@ -21,7 +21,7 @@ import jxl.Workbook;
 import jxl.read.biff.BiffException;
 
 public class BookingFacilityhelpdemo12 extends BookingFacilityHelper{
-	WebDriver driver = new FirefoxDriver();
+	//WebDriver driver = new FirefoxDriver();
 	int year = 2016;
 	int month = 9;
 	int day = 10;
@@ -35,99 +35,7 @@ public class BookingFacilityhelpdemo12 extends BookingFacilityHelper{
 	}
 	
 	
-	public void BookFacility(WebDriver driver) throws InterruptedException, BiffException, IOException {
-		Thread.sleep(2000);
-		File fs = new File("C:/Users/Swetha/Desktop/Financial Scenarios Test data for DEMO12/Copy of Generate Fixed Invoice.xls");
-		Workbook ws = Workbook.getWorkbook(fs);
-		Sheet s = ws.getSheet(3);
-		int rows = s.getRows();
-		int columns = s.getColumns();
-		String inputdata[][] = new String[rows][columns];
-		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < columns; j++) {
-				Cell cl = s.getCell(j, i);
-				inputdata[i][j] = cl.getContents();
-			}
-		}
-		//String[][] bookeddate = new String[1][1];
-		for (int i = 0; i < rows; i++) {
-			String URL1 = inputdata[i][0];
-			String facilityname1= inputdata[i][1];
-			String fromtime1 =inputdata[i][2];
-			String totime1 =inputdata[i][3];
-			String blockno1 =inputdata[i][4];
-			String apartmentno1 = inputdata[i][5];
-			String description1 =inputdata[i][6];
-		driver.navigate().to(URL1);
-		Thread.sleep(4000);
-		driver.findElement(By.id("searchgrid")).click(); // search
-		Thread.sleep(4000);
-		driver.findElement(By.xpath("//option[contains(.,'Facility Name')]")).click();
-		Thread.sleep(4000);
-		driver.findElement(By.id("jqg1")).sendKeys(facilityname1);
-		Thread.sleep(4000);
-		driver.findElement(By.id("fbox_Grid_search")).click();// find button
-		Thread.sleep(4000);
-		driver.findElement(By.xpath(".//*[@id='searchhdfbox_Grid']/a/span")).click(); // close
-																						// button
-		Thread.sleep(4000);
-		driver.findElement(By.xpath(".//*[@id='1']/td[12]/a/button")).click();
-		Thread.sleep(4000);
-		driver.findElement(By.id("Add")).click();
-		Thread.sleep(5000);
-		Calendar c = Calendar.getInstance();
-		c.set(Calendar.YEAR, year);
-		c.set(Calendar.MONTH, month);
-		c.set(Calendar.DAY_OF_MONTH, day);
-		DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
-		Date datespecified = c.getTime();
-		driver.findElement(By.id("FromDate")).clear();
-		Thread.sleep(2000);
-		driver.findElement(By.id("FromDate")).sendKeys(df.format(datespecified));
-		Thread.sleep(2000);
-		//String bookeddate1 = driver.findElement(By.id("FromDate")).getText();
-		//bookeddate [0][0]=bookeddate1;
-		driver.findElement(By.id("FromTime")).clear();
-		Thread.sleep(2000);
-		driver.findElement(By.id("FromTime")).sendKeys(fromtime1);
-		Thread.sleep(2000);
-		driver.findElement(By.id("ToDate")).clear();
-		Thread.sleep(2000);
-		driver.findElement(By.id("ToDate")).sendKeys(df.format(datespecified));
-		Thread.sleep(2000);
-		driver.findElement(By.xpath(".//*[@id='FacilityBooking']/div/div[2]/div/div/div/div/div[2]")).click();
-		Thread.sleep(5000);
-		driver.findElement(By.id("ToTime")).clear();
-		Thread.sleep(2000);
-		driver.findElement(By.id("ToTime")).sendKeys(totime1);
-		Thread.sleep(2000);
-		driver.findElement(By.id("auto_BlockID")).clear();
-		Thread.sleep(2000);
-		driver.findElement(By.id("auto_BlockID")).sendKeys(blockno1);
-		Thread.sleep(2000);
-		driver.findElement(By.id("auto_ApartmentID")).sendKeys(apartmentno1);
-		Thread.sleep(3000);
-		driver.findElement(By.id("Description")).sendKeys(description1);
-		Thread.sleep(2000);
-		driver.findElement(By.xpath(".//*[@id='FacilityBooking']/div/div[3]/button[2]")).submit();
-		Thread.sleep(2000);
-		Alert alert2 = driver.switchTo().alert();
-		Thread.sleep(2000);
-		String message3 = alert2.getText();
-		System.out.println(message3);
-		Thread.sleep(4000);
-		alert2.accept();
-		Thread.sleep(2000);
-		WebElement header = driver.findElement(By.xpath("html/body/div[1]/div/div/div/div[1]/h3"));
-		String value = header.getText();
-		Assert.assertEquals(value, "Facility Availability");
-		Thread.sleep(2000);
-		System.out.println("Facility booked successfully Invoice no. generated");
-		}
-		
-		
-		
-	}
+	
 	public String ToVerifyGeneratedVoucherno(WebDriver driver) throws InterruptedException {
 		driver.navigate().to("https://test-itsmyaccount.azurewebsites.net/Voucher");
 		Thread.sleep(4000);
