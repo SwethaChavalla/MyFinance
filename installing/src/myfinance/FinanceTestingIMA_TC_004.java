422f3ae6eba0246c93e045533eb5d229d660df17
Swetha Chavalla <swethachavalla@gmail.com>
2016-10-26 10:56:53 +0530

    Added all the methods under one scenario.

--- installing/src/myfinance/FinanceTestingIMA_TC_004.java
+++ installing/src/myfinance/FinanceTestingIMA_TC_004.java
@@ -15,41 +15,72 @@ import jxl.read.biff.BiffException;
 
 public class FinanceTestingIMA_TC_004 {
 	WebDriver driver = new FirefoxDriver();
-	String advancevoucherno = "AFC-110000582" ;
+	String advancevoucherno;
 	logindetails ldr = new logindetails();
+	BookingFacilityHelper help1 = new BookingFacilityhelpdemo12();
 	FinancialVouchersHelp help2 = new FinancialVouchersHelp();
-	
+
 	@Test(priority = 1, dataProvider = "logintestdata")
-	public void Login(String URL,String username, String password) throws InterruptedException {
-		ldr.adminlogin(driver,URL, username, password);
+	public void Login(String URL, String username, String password) throws InterruptedException {
+		ldr.adminlogin(driver, URL, username, password);
 		Thread.sleep(2000);
 	}
-	@Test(priority=2)
-	public void AdvancePayment() throws BiffException, InterruptedException, IOException{
-	  advancevoucherno =  help2.advancepayment(driver);
-	  System.out.println(advancevoucherno);
-	
-	
+
+	@Test(priority = 2)
+	public void AdvancePaymentFacility() throws BiffException, InterruptedException, IOException {
+			advancevoucherno = help2.AdvancePaymentForFacility(driver);
+			System.out.println(advancevoucherno);
 	}
-	@Test(priority=3)
-	public void ReverseFacilityAdvance() throws InterruptedException{
+
+	
+	 @Test(priority = 3) public void AdvancePaymentForGeneralandMaintenance()
+	 throws InterruptedException, BiffException, IOException {
+	 help2.AdvancePaymentForGeneralAndMaintenance(driver); 
+	 }
+	
+
+	@Test(priority = 4)
+	public void ReverseFacilityAdvance() throws InterruptedException, BiffException, IOException {
 		help2.ReverseAdvance(driver, advancevoucherno);
 	}
-	@DataProvider    (name = "logintestdata")
-    public  Object [][] readexcel()  throws  IOException, BiffException  {
-       File fs = new File("C:/Users/Swetha/Desktop/IMA Testing/All Financial Scenarios Test Data.xls");
-        Workbook ws= Workbook.getWorkbook(fs);
-        Sheet s = ws.getSheet("Logindata");
-        int rows = s.getRows();
-        int columns = s.getColumns();
-        String inputdata [][]= new String [rows-1][columns]; 
-        for (int i=1; i<rows; i++){
-            for (int j=0; j<columns; j++){
-                Cell cl = s.getCell(j,i);
-                inputdata [i-1][j] = cl.getContents();
-                      
-            }
-        }
-                return inputdata;
+
+	@Test(priority = 5)
+	public void AdvancePaymentFacilityAfterReversing() throws BiffException, InterruptedException, IOException {
+		help2.AdvancePaymentForFacility(driver);
+
+	}
+
+	@Test(priority = 6)
+	public void Addfacility() throws BiffException, InterruptedException, IOException {
+		help1.Addfacility(driver);
+
+	}
+
+	@Test(priority = 7)
+	public void Bookfacility() throws BiffException, InterruptedException, IOException {
+		help1.BookFacility(driver);
+	}
+
+	@Test(priority = 8)
+	public void FIGvoucherno() throws InterruptedException, BiffException, IOException {
+		help1.ToVerifyGeneratedVoucherno(driver);
+	}
+
+	@DataProvider(name = "logintestdata")
+	public Object[][] readexcel() throws IOException, BiffException {
+		File fs = new File("C:/Users/Swetha/Desktop/IMA Testing/All Financial Scenarios Test Data.xls");
+		Workbook ws = Workbook.getWorkbook(fs);
+		Sheet s = ws.getSheet("Logindata");
+		int rows = s.getRows();
+		int columns = s.getColumns();
+		String inputdata[][] = new String[rows - 1][columns];
+		for (int i = 1; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				Cell cl = s.getCell(j, i);
+				inputdata[i - 1][j] = cl.getContents();
+
+			}
+		}
+		return inputdata;
 	}
 }
