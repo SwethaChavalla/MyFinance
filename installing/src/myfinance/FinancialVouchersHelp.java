4612cb025664a274035ad6c5d663bab51cb9643d
Swetha Chavalla <swethachavalla@gmail.com>
2016-10-26 10:55:28 +0530

    Reading data from the sheet for this method "ReverseAdvance"

--- installing/src/myfinance/FinancialVouchersHelp.java
+++ installing/src/myfinance/FinancialVouchersHelp.java
@@ -16,12 +16,10 @@ import jxl.read.biff.BiffException;
 
 public class FinancialVouchersHelp {
 
-	
-	
 	// WebDriver driver = new FirefoxDriver();
 	String voucherno;
 	String advanceamount;
-	
+
 	String editamount1 = "400";
 	String editamount2 = "600";
 
@@ -29,221 +27,267 @@ public class FinancialVouchersHelp {
 	 * public void login() throws InterruptedException { logindetails logd = new
 	 * logindetails(); logd.adminlogin(driver, "DEMO_9", "DEMO_9"); }
 	 */
-	public Object[][] Payment(WebDriver driver, String voucherno) throws InterruptedException, BiffException, IOException {
+	public Object[][] Payment(WebDriver driver, String voucherno)
+			throws InterruptedException, BiffException, IOException {
 		String x = "3";
 		String y = "d";
-		
-		x=y;
-		
-		String  splitmessage[][] = new String[1][1] ;
+
+		x = y;
+
+		String splitmessage[][] = new String[1][1];
 		Thread.sleep(4000);
 		File fs = new File("C:/Users/Swetha/Desktop/IMA Testing/All Financial Scenarios Test Data.xls");
 		Workbook ws = Workbook.getWorkbook(fs);
 		Sheet s = ws.getSheet("Payment");
 		int rows = s.getRows();
 		int columns = s.getColumns();
-		String inputdata[][] = new String[rows-1][columns];
+		String inputdata[][] = new String[rows - 1][columns];
 		for (int i = 1; i < rows; i++) {
 			for (int j = 0; j < columns; j++) {
 				Cell cl = s.getCell(j, i);
-				inputdata[i-1][j] = cl.getContents();
+				inputdata[i - 1][j] = cl.getContents();
 			}
 		}
-		for (int i=0; i<rows-1; i++){
+		for (int i = 0; i < rows - 1; i++) {
 			String url = inputdata[i][0];
 			String Block = inputdata[i][1];
-			String Flatno =inputdata[i][2];
+			String Flatno = inputdata[i][2];
 			String naration = inputdata[i][3];
-		driver.navigate().to(url);
-		Thread.sleep(4000);
-		driver.findElement(By.id("Payment")).click();
-		Thread.sleep(4000);
-		setDate(driver);
-		driver.findElement(By.xpath(".//*[@id='Payment']/div/div/div[2]/div[2]/div[1]/div/a/i")).click();
-		Thread.sleep(4000);
-		driver.findElement(By.linkText(Block)).click();
-		Thread.sleep(4000);
-		driver.findElement(By.xpath(".//*[@id='Payment']/div/div/div[2]/div[2]/div[2]/div/a/i")).click();
-		//*[@id='Payment']/div/div/div[2]/div[2]/div[2]/div/a/i
-		Thread.sleep(4000);
-		driver.findElement(By.linkText(Flatno)).click();
-		Thread.sleep(4000);
-		driver.findElement(By.id("Go")).click();
-		Thread.sleep(4000);
-		WebElement paymenttable = driver.findElement(By.xpath(".//*[@id='Payment']/div/div/div[2]/div[4]"));
-		//*[@id='Payment']/div/div/div[2]/div[4]/table/tbody
-		//*[@id='Payment']/div/div/div[2]/div[3]/table/tbody
-		List<WebElement> rows1 = paymenttable.findElements(By.tagName("tr"));
-		int rowscount = rows1.size();
-		for (rowscount = 1; rowscount < rows1.size(); rowscount++) {
-			List<WebElement> columns1 = rows1.get(rowscount).findElements(By.tagName("td"));
-			String rowvalue = columns1.get(1).getText();
-			if (rowvalue.equals(voucherno)) {
-				System.out.println("Paying amount for the voucher: "  + voucherno);
-				columns1.get(0).findElement(By.tagName("input")).click();
-				break;
+			driver.navigate().to(url);
+			Thread.sleep(4000);
+			driver.findElement(By.id("Payment")).click();
+			Thread.sleep(4000);
+			setDate(driver);
+			driver.findElement(By.xpath(".//*[@id='Payment']/div/div/div[2]/div[2]/div[1]/div/a/i")).click();
+			Thread.sleep(4000);
+			driver.findElement(By.linkText(Block)).click();
+			Thread.sleep(4000);
+			driver.findElement(By.xpath(".//*[@id='Payment']/div/div/div[2]/div[2]/div[2]/div/a/i")).click();
+			// *[@id='Payment']/div/div/div[2]/div[2]/div[2]/div/a/i
+			Thread.sleep(4000);
+			driver.findElement(By.linkText(Flatno)).click();
+			Thread.sleep(4000);
+			driver.findElement(By.id("Go")).click();
+			Thread.sleep(4000);
+			WebElement paymenttable = driver.findElement(By.xpath(".//*[@id='Payment']/div/div/div[2]/div[4]"));
+			// *[@id='Payment']/div/div/div[2]/div[4]/table/tbody
+			// *[@id='Payment']/div/div/div[2]/div[3]/table/tbody
+			List<WebElement> rows1 = paymenttable.findElements(By.tagName("tr"));
+			int rowscount = rows1.size();
+			for (rowscount = 1; rowscount < rows1.size(); rowscount++) {
+				List<WebElement> columns1 = rows1.get(rowscount).findElements(By.tagName("td"));
+				String rowvalue = columns1.get(1).getText();
+				if (rowvalue.equals(voucherno)) {
+					System.out.println("Paying amount for the voucher: " + voucherno);
+					columns1.get(0).findElement(By.tagName("input")).click();
+					break;
+				}
 			}
+			Thread.sleep(4000);
+			driver.findElement(By.id("Narration")).sendKeys(naration);
+			Thread.sleep(4000);
+			driver.findElement(By.id("btnpay")).click();
+			Thread.sleep(4000);
+			Alert alert = driver.switchTo().alert();
+			Thread.sleep(4000);
+			String message3 = alert.getText();
+			Thread.sleep(4000);
+			System.out.println(message3);
+			splitmessage[0][0] = message3.split(" ")[1];
+			Thread.sleep(4000);
+			System.out.println("Generated Payment Receipt no: " + splitmessage[0][0]);
+			alert.accept();
+			Thread.sleep(2000);
+
 		}
-		Thread.sleep(4000);
-		driver.findElement(By.id("Narration")).sendKeys(naration);
-		Thread.sleep(4000);
-		driver.findElement(By.id("btnpay")).click();
-		Thread.sleep(4000);
-		Alert alert = driver.switchTo().alert();
-		Thread.sleep(4000);
-		String message3 = alert.getText();
-		Thread.sleep(4000);
-		System.out.println(message3);
-		splitmessage[0][0] = message3.split(" ")[1];
-		Thread.sleep(4000);
-		System.out.println("Generated Payment Receipt no: "  + splitmessage[0][0]);
-		alert.accept();
-		Thread.sleep(2000);
-		
-	}
 		return splitmessage;
 	}
 
-	public void setDate(WebDriver driver) throws InterruptedException{
+	public void setDate(WebDriver driver) throws InterruptedException {
 		System.out.println("Empty setDate");
 	}
-	
-	public String advancepayment(WebDriver driver) throws InterruptedException, BiffException, IOException {
+
+	public String AdvancePaymentForFacility(WebDriver driver) throws InterruptedException, BiffException, IOException {
 		String advancevoucherno = null;
 		File fs = new File("C:/Users/Swetha/Desktop/IMA Testing/All Financial Scenarios Test Data.xls");
 		Workbook ws = Workbook.getWorkbook(fs);
 		Sheet s = ws.getSheet("AdvancePaymentData");
 		int rows = s.getRows();
 		int columns = s.getColumns();
-		String inputdata[][] = new String[rows-1][columns];
+		String inputdata[][] = new String[rows - 1][columns];
 		for (int i = 1; i < rows; i++) {
 			for (int j = 0; j < columns; j++) {
 				Cell cl = s.getCell(j, i);
-				inputdata[i-1][j] = cl.getContents();
+				inputdata[i - 1][j] = cl.getContents();
 			}
 		}
-		for (int i=0; i<rows-1; i++){
+		System.out.println(rows);
+		for (int i = 0; i < rows - 1; i++) {
+			System.out.println(i);
 			String url = inputdata[i][0];
 			String Block = inputdata[i][1];
-			String Flatno =inputdata[i][2];
+			String Flatno = inputdata[i][2];
 			String vouchertype = inputdata[i][3];
 			String advanceamount = inputdata[i][4];
 			String naration = inputdata[i][5];
 			String message = inputdata[i][6];
 			driver.navigate().to(url);
-		Thread.sleep(2000);
-		driver.findElement(By.id("Payment")).click(); // payment button
-		Thread.sleep(4000);
-		driver.findElement(By.cssSelector("#auto_BlockID")).clear();// Block ID
-																	// dropdown
-		Thread.sleep(4000);
-		driver.findElement(By.cssSelector("#auto_BlockID")).sendKeys(Block);
-		Thread.sleep(2000);
-		driver.findElement(By.id("auto_ApartmentId")).sendKeys(Flatno); // Apartment
+			Thread.sleep(2000);
+			driver.findElement(By.id("Payment")).click(); // payment button
+			Thread.sleep(4000);
+			driver.findElement(By.cssSelector("#auto_BlockID")).clear();// Block
 																		// ID
-		Thread.sleep(4000);
-		driver.findElement(By.cssSelector("#auto_InvType")).clear();
-		Thread.sleep(2000);
-		driver.findElement(By.cssSelector("#auto_InvType")).sendKeys(vouchertype); // Voucher
-																					// type
-		Thread.sleep(4000);
-		driver.findElement(By.id("Go")).click();
-		Thread.sleep(4000);
-		driver.findElement(By.xpath(".//*[@id='advrow']/td[4]/input")).clear();
-		Thread.sleep(2000);
-		driver.findElement(By.xpath(".//*[@id='advrow']/td[4]/input")).sendKeys(advanceamount);
-		Thread.sleep(2000);
-		driver.findElement(By.id("Narration")).sendKeys(naration);
-		Thread.sleep(2000);
-		driver.findElement(By.id("btnpay")).click(); // pay button
-		Thread.sleep(2000);
-		Alert alert = driver.switchTo().alert();
-		Thread.sleep(2000);
-		String vouchermessage = alert.getText();
-		Thread.sleep(2000);
-		alert.accept();
-		Thread.sleep(2000);
-		String advancevoucherno1 = vouchermessage.split(" ")[1];
-		System.out.println(message  +   advancevoucherno1);
-		 advancevoucherno = advancevoucherno1;
+																		// dropdown
+			Thread.sleep(4000);
+			driver.findElement(By.cssSelector("#auto_BlockID")).sendKeys(Block);
+			Thread.sleep(2000);
+			driver.findElement(By.id("auto_ApartmentId")).sendKeys(Flatno); // Apartment
+																			// ID
+			Thread.sleep(4000);
+			driver.findElement(By.cssSelector("#auto_InvType")).clear();
+			Thread.sleep(2000);
+			driver.findElement(By.cssSelector("#auto_InvType")).sendKeys(vouchertype); // Voucher
+																						// type
+			Thread.sleep(4000);
+			driver.findElement(By.id("Go")).click();
+			Thread.sleep(4000);
+			driver.findElement(By.xpath(".//*[@id='advrow']/td[4]/input")).clear();
+			Thread.sleep(2000);
+			driver.findElement(By.xpath(".//*[@id='advrow']/td[4]/input")).sendKeys(advanceamount);
+			Thread.sleep(2000);
+			driver.findElement(By.id("Narration")).sendKeys(naration);
+			Thread.sleep(2000);
+			driver.findElement(By.id("btnpay")).click(); // pay button
+			Thread.sleep(2000);
+			Alert alert = driver.switchTo().alert();
+			Thread.sleep(2000);
+			String vouchermessage = alert.getText();
+			Thread.sleep(2000);
+			alert.accept();
+			Thread.sleep(2000);
+			String advancevoucherno1 = vouchermessage.split(" ")[1];
+			System.out.println(message + advancevoucherno1);
+			advancevoucherno = advancevoucherno1;
+			System.out.println("inside loop " + advancevoucherno);
 		}
-		System.out.println(advancevoucherno);
-		return advancevoucherno;
-	}
-	
-	
-	public String advancepaymentgeneral(WebDriver driver, String url, String Block, String Flat, String advanceamount,
-			String narration) throws InterruptedException {
-		driver.navigate().to(url);
-		Thread.sleep(2000);
-		driver.findElement(By.id("Payment")).click(); // payment button
-		Thread.sleep(4000);
-		driver.findElement(By.cssSelector("#auto_BlockID")).clear();// Block ID
-																	// dropdown
-		Thread.sleep(4000);
-		driver.findElement(By.cssSelector("#auto_BlockID")).sendKeys(Block);
-		Thread.sleep(2000);
-		driver.findElement(By.id("auto_ApartmentId")).sendKeys(Flat); // Apartment
-																		// ID
-		Thread.sleep(4000);
-		driver.findElement(By.id("Go")).click();
-		Thread.sleep(4000);
-		driver.findElement(By.xpath(".//*[@id='advrow']/td[4]/input")).clear();
-		Thread.sleep(2000);
-		driver.findElement(By.xpath(".//*[@id='advrow']/td[4]/input")).sendKeys(advanceamount);
-		Thread.sleep(2000);
-		driver.findElement(By.id("Narration")).sendKeys(narration);
-		Thread.sleep(2000);
-		driver.findElement(By.id("btnpay")).click(); // pay button
-		Alert alert = driver.switchTo().alert();
-		String vouchermessage = alert.getText();
-		alert.accept();
-		String advancevoucherno = vouchermessage.split(" ")[1];
-		System.out.println("advanceGeneralVoucherno:  " + advancevoucherno);
+		System.out.println("outside loop " + advancevoucherno);
 		return advancevoucherno;
 	}
 
-	public void ReverseAdvance(WebDriver driver, String advancevoucherno) throws InterruptedException {
+	public void AdvancePaymentForGeneralAndMaintenance(WebDriver driver)
+			throws InterruptedException, BiffException, IOException {
+
+		File fs = new File("C:/Users/Swetha/Desktop/IMA Testing/All Financial Scenarios Test Data.xls");
+		Workbook ws = Workbook.getWorkbook(fs);
+		Sheet s = ws.getSheet("AdvancePaymentDataForG&M");
+		int rows = s.getRows();
+		int columns = s.getColumns();
+		String inputdata[][] = new String[rows - 1][columns];
+		for (int i = 1; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				Cell cl = s.getCell(j, i);
+				inputdata[i - 1][j] = cl.getContents();
+			}
+		}
+		for (int i = 0; i < rows - 1; i++) {
+			String url = inputdata[i][0];
+			String Block = inputdata[i][1];
+			String Flatno = inputdata[i][2];
+			String vouchertype = inputdata[i][3];
+			String advanceamount = inputdata[i][4];
+			String naration = inputdata[i][5];
+			String message = inputdata[i][6];
+			driver.navigate().to(url);
+			Thread.sleep(2000);
+			driver.navigate().to(url);
+			Thread.sleep(2000);
+			driver.findElement(By.id("Payment")).click(); // payment button
+			Thread.sleep(4000);
+			driver.findElement(By.cssSelector("#auto_BlockID")).clear();// Block
+																		// ID
+																		// dropdown
+			Thread.sleep(4000);
+			driver.findElement(By.cssSelector("#auto_BlockID")).sendKeys(Block);
+			Thread.sleep(2000);
+			driver.findElement(By.id("auto_ApartmentId")).sendKeys(Flatno); // Apartment
+																			// ID
+			Thread.sleep(4000);
+			driver.findElement(By.cssSelector("#auto_InvType")).clear();
+			Thread.sleep(2000);
+			driver.findElement(By.cssSelector("#auto_InvType")).sendKeys(vouchertype); // Voucher
+																						// type
+			Thread.sleep(4000);
+			driver.findElement(By.id("Go")).click();
+			Thread.sleep(4000);
+			driver.findElement(By.xpath(".//*[@id='advrow']/td[4]/input")).clear();
+			Thread.sleep(2000);
+			driver.findElement(By.xpath(".//*[@id='advrow']/td[4]/input")).sendKeys(advanceamount);
+			Thread.sleep(2000);
+			driver.findElement(By.id("Narration")).sendKeys(naration);
+			Thread.sleep(2000);
+			driver.findElement(By.id("btnpay")).click(); // pay button
+			Alert alert = driver.switchTo().alert();
+			String vouchermessage = alert.getText();
+			alert.accept();
+			String advancevoucherno1 = vouchermessage.split(" ")[1];
+			System.out.println(message + advancevoucherno1);
+		}
+
+	}
+
+	public void ReverseAdvance(WebDriver driver, String advancevoucherno) throws InterruptedException, BiffException, IOException {
 		String reverseadvancevoucherno = null;
-		driver.navigate().to("https://test-itsmyaccount.azurewebsites.net/Voucher");
+		File fs = new File("C:/Users/Swetha/Desktop/IMA Testing/All Financial Scenarios Test Data.xls");
+		Workbook ws = Workbook.getWorkbook(fs);
+		Sheet s = ws.getSheet("ReverseAdvanceURL");
+		int rows = s.getRows();
+		int columns = s.getColumns();
+		String inputdata[][] = new String[rows - 1][columns];
+		for (int i = 1; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				Cell cl = s.getCell(j, i);
+				inputdata[i - 1][j] = cl.getContents();
+			}
+		}
+		for (int x = 0; x < rows - 1; x++) {
+			String url = inputdata[x][0];
+		driver.navigate().to(url);
 		Thread.sleep(2000);
 		driver.findElement(By.id("RevAdv")).click();
 		Thread.sleep(4000);
 		WebElement reversaltable = driver.findElement(By.xpath(".//*[@id='Grid1']/tbody"));
 		Thread.sleep(2000);
 		int rowcount = reversaltable.findElements(By.tagName("tr")).size();
-		System.out.println(rowcount);
 		Thread.sleep(2000);
 		loop1: for (int i = 1; i < rowcount; i++) {
 			String rowID = i + "";
-			//List<WebElement> rows = reversaltable.findElement(By.xpath(".//*[@id='Grid1']/tbody")).findElements(By.id(rowID));
-			List<WebElement> rows = reversaltable.findElements(By.id(rowID));
+			// List<WebElement> rows =
+			// reversaltable.findElement(By.xpath(".//*[@id='Grid1']/tbody")).findElements(By.id(rowID));
+			List<WebElement> rows1 = reversaltable.findElements(By.id(rowID));
 			Thread.sleep(2000);
-			String rowdata = rows.get(0).findElements(By.tagName("td")).get(6).getText();
+			String rowdata = rows1.get(0).findElements(By.tagName("td")).get(6).getText();
 			if (rowdata.equals(advancevoucherno)) {
-				rows.get(0).findElements(By.tagName("td")).get(6).click();
+				rows1.get(0).findElements(By.tagName("td")).get(6).click();
 				driver.findElement(By.id("Reason")).sendKeys("Reversing advance amount of facility booking");
 				driver.findElement(By.id("Generate")).click();
 				Alert alert = driver.switchTo().alert();
 				String message = alert.getText();
-				 reverseadvancevoucherno = message.split(" ")[3];
+				reverseadvancevoucherno = message.split(" ")[3];
 				System.out.println("Reverse advance voucher no: " + reverseadvancevoucherno);
 				alert.accept();
 				break loop1;
-				
-			} 
-		}
-		if (reverseadvancevoucherno == null){
-				driver.findElement(By.xpath(".//*[@id='Popup']/div/div/div[3]/div[3]/button[2]")).click();
-				System.out.println("Reversal is not possible for the voucher:" + advancevoucherno);
-		}
-		}
-		
-		
-	
 
-	public void Paymentforscenario4forcancel(WebDriver driver,String URL,String Block,String Flat, String advancevoucher, String editamount,String narration)
-			throws InterruptedException {
+			}
+		}
+		if (reverseadvancevoucherno == null) {
+			driver.findElement(By.xpath(".//*[@id='Popup']/div/div/div[3]/div[3]/button[2]")).click();
+			System.out.println("Reversal is not possible for the voucher:" + advancevoucherno);
+		}
+		}
+	}
+
+	public void Paymentforscenario4forcancel(WebDriver driver, String URL, String Block, String Flat,
+			String advancevoucher, String editamount, String narration) throws InterruptedException {
 		driver.navigate().to(URL);
 		Thread.sleep(2000);
 		driver.findElement(By.id("Payment")).click();
@@ -286,8 +330,8 @@ public class FinancialVouchersHelp {
 
 	}
 
-	public void Paymentforscenario4forpay(WebDriver driver,String URL,String Block,String Flat,String advancevoucher,String editamount, String narration)
-			throws InterruptedException {
+	public void Paymentforscenario4forpay(WebDriver driver, String URL, String Block, String Flat,
+			String advancevoucher, String editamount, String narration) throws InterruptedException {
 		driver.navigate().to(URL);
 		Thread.sleep(2000);
 		driver.findElement(By.id("Payment")).click();
@@ -455,5 +499,4 @@ public class FinancialVouchersHelp {
 		}
 	}
 
-	
 }
