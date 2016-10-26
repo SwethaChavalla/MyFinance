9fe0dbb8d30e28458b058d86b4e051e99f0ae76e
Swetha Chavalla <swethachavalla@gmail.com>
2016-10-26 11:55:35 +0530

    Added this method SelectTaxInProfile

--- installing/src/myfinance/InvoiceGenerationhelpdemo12.java
+++ installing/src/myfinance/InvoiceGenerationhelpdemo12.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import jxl.Cell;
 import jxl.Sheet;
@@ -20,7 +21,33 @@ import jxl.read.biff.BiffException;
 
 
 public class InvoiceGenerationhelpdemo12 extends InvoiceGenerationHelper {
-	
+	WebDriver driver = new FirefoxDriver();
+
+	public void SelectTaxInProfile() throws InterruptedException, BiffException, IOException{
+		File fs = new File("C:/Users/Swetha/Desktop/IMA Testing/All Financial Scenarios Test Data.xls");
+		Workbook ws = Workbook.getWorkbook(fs);
+		Sheet s = ws.getSheet("ServiceTaxPercentage");
+		int rows = s.getRows();
+		int columns = s.getColumns();
+		String inputdata[][] = new String[rows - 1][columns];
+		for (int i = 1; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				Cell cl = s.getCell(j, i);
+				inputdata[i - 1][j] = cl.getContents();
+			}
+		}
+		for (int x = 0; x < rows - 1; x++) {
+			String url = inputdata[x][0];
+			String percentage = inputdata[x][1];
+		driver.navigate().to(url);
+		driver.findElement(By.id("ServiceTax")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.id("ServiceTaxPercent")).sendKeys(percentage);
+		Thread.sleep(2000);
+		driver.findElement(By.id("btnSave")).click();
+		ldr.logout(driver);
+		}
+		}
 	
 }
 
