f59b250a98f8850a3b92a1179535091b3f43058e
Swetha Chavalla <swethachavalla@gmail.com>
2016-10-26 11:52:47 +0530

    Read data from the sheet for this method SelectTaxInProfile

--- installing/src/myfinance/FinancialVouchersDEMO12.java
+++ installing/src/myfinance/FinancialVouchersDEMO12.java
@@ -44,16 +44,31 @@ public class FinancialVouchersDEMO12 extends FinancialVouchersHelp{
 		Thread.sleep(2000);
 	}
    
-	public void SelectTaxInProfile() throws InterruptedException{
-		driver.navigate().to("https://test-itsmyaccount.azurewebsites.net/SocietySetup/Step2/?Type=edit");
+	public void SelectTaxInProfile() throws InterruptedException, BiffException, IOException{
+		File fs = new File("C:/Users/Swetha/Desktop/IMA Testing/All Financial Scenarios Test Data.xls");
+		Workbook ws = Workbook.getWorkbook(fs);
+		Sheet s = ws.getSheet("ServiceTaxPercentage");
+		int rows = s.getRows();
+		int columns = s.getColumns();
+		String inputdata[][] = new String[rows - 1][columns];
+		for (int i = 1; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				Cell cl = s.getCell(j, i);
+				inputdata[i - 1][j] = cl.getContents();
+			}
+		}
+		for (int x = 0; x < rows - 1; x++) {
+			String url = inputdata[x][0];
+			String percentage = inputdata[x][1];
+		driver.navigate().to(url);
 		driver.findElement(By.id("ServiceTax")).click();
 		Thread.sleep(2000);
-		driver.findElement(By.id("ServiceTaxPercent")).sendKeys("10");
+		driver.findElement(By.id("ServiceTaxPercent")).sendKeys(percentage);
 		Thread.sleep(2000);
 		driver.findElement(By.id("btnSave")).click();
 		ldr.logout(driver);
 		
-		
+		}
 	}
 	
 	
