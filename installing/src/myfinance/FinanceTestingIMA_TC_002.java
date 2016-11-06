98fae53f7dff4388f08ea8fb9177d18b2cff6520
Swetha Chavalla <swethachavalla@gmail.com>
2016-11-06 22:03:02 +0530

    Passing parameters

--- installing/src/myfinance/FinanceTestingIMA_TC_002.java
+++ installing/src/myfinance/FinanceTestingIMA_TC_002.java
@@ -30,19 +30,23 @@ public void Login(String URL,String username, String password) throws Interrupte
 	ldr.adminlogin(driver,URL, username, password);
 	Thread.sleep(2000);
 }
-@DataProvider(name="FixedInvoiceno")
-public Object[][] FixedInvoiceGenerationWithDates() throws BiffException, InterruptedException, IOException{
-	 return help1.GenerateFixedInvoiceno(driver);
+@Test(priority=2,dataProvider="FixedInvoice")
+public void FixedInvoiceGenerationWithDates(String URL,String Invoicedateid,String Duedateid,String apartment
+		,String Block,String amount,String narration,String URL2)  throws BiffException, InterruptedException, IOException{
+	splitmessage = help1.GenerateFixedInvoiceno(driver, URL, Invoicedateid, Duedateid, apartment
+				, Block, amount, narration, URL2) ;
 	
 }
-@Test (priority=2)
-public void GenerateFixedInvoiceTenant() throws BiffException, InterruptedException, IOException{
-	help1.GenerateFixedInvoicenoForTenant(driver);
+@Test (priority=4,dataProvider="FixedInvoiceForTenant")
+public void GenerateFixedInvoiceTenant(String URL,String Invoicedateid,String Duedateid,String apartment
+		,String Block,String amount,String narration,String URL2) throws BiffException, InterruptedException, IOException{
+	help1.GenerateFixedInvoicenoForTenant(driver, URL, Invoicedateid, Duedateid, apartment
+			, Block, amount, narration, URL2);
 }
 
-@Test(priority=3,dataProvider="FixedInvoiceno")
-public void ReverseFixedInvoice(String splitmessage) throws InterruptedException, BiffException, IOException{
-	help1.ReverseInvoice(driver, splitmessage);
+@Test(priority=3,dataProvider="ReverseInvoice")
+public void ReverseFixedInvoice(String url,String dateid,String url2) throws InterruptedException, BiffException, IOException{
+	help1.ReverseInvoice(driver,url, splitmessage,dateid,url2);
 }
 
 @DataProvider    (name = "logintestdata")
@@ -50,6 +54,60 @@ public  Object [][] readexcel()  throws  IOException, BiffException  {
    File fs = new File("C:/Users/Swetha/Desktop/IMA Testing/All Financial Scenarios Test Data.xls");
     Workbook ws= Workbook.getWorkbook(fs);
     Sheet s = ws.getSheet("Logindata");
+    int rows = s.getRows();
+    int columns = s.getColumns();
+    String inputdata [][]= new String [rows-1][columns]; 
+    for (int i=1; i<rows; i++){
+        for (int j=0; j<columns; j++){
+            Cell cl = s.getCell(j,i);
+            inputdata [i-1][j] = cl.getContents();
+                  
+        }
+    }
+            return inputdata;
+}
+
+@DataProvider    (name = "FixedInvoice")
+public  Object [][] readexcel1()  throws  IOException, BiffException  {
+   File fs = new File("C:/Users/Swetha/Desktop/IMA Testing/FinanceTestingIMA_TC_002.xls");
+    Workbook ws= Workbook.getWorkbook(fs);
+    Sheet s = ws.getSheet("FixedInvoiceGeneration");
+    int rows = s.getRows();
+    int columns = s.getColumns();
+    String inputdata [][]= new String [rows-1][columns]; 
+    for (int i=1; i<rows; i++){
+        for (int j=0; j<columns; j++){
+            Cell cl = s.getCell(j,i);
+            inputdata [i-1][j] = cl.getContents();
+                  
+        }
+    }
+            return inputdata;
+}
+
+@DataProvider    (name = "ReverseInvoice")
+public  Object [][] readexcel2()  throws  IOException, BiffException  {
+   File fs = new File("C:/Users/Swetha/Desktop/IMA Testing/FinanceTestingIMA_TC_002.xls");
+    Workbook ws= Workbook.getWorkbook(fs);
+    Sheet s = ws.getSheet("ReverseInvoice");
+    int rows = s.getRows();
+    int columns = s.getColumns();
+    String inputdata [][]= new String [rows-1][columns]; 
+    for (int i=1; i<rows; i++){
+        for (int j=0; j<columns; j++){
+            Cell cl = s.getCell(j,i);
+            inputdata [i-1][j] = cl.getContents();
+                  
+        }
+    }
+            return inputdata;
+}
+
+@DataProvider    (name = "FixedInvoiceForTenant")
+public  Object [][] readexcel3()  throws  IOException, BiffException  {
+   File fs = new File("C:/Users/Swetha/Desktop/IMA Testing/FinanceTestingIMA_TC_002.xls");
+    Workbook ws= Workbook.getWorkbook(fs);
+    Sheet s = ws.getSheet("FixedInvoiceGenerationTenant");
     int rows = s.getRows();
     int columns = s.getColumns();
     String inputdata [][]= new String [rows-1][columns]; 
